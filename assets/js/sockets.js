9804b1c90612e8e80f47b5b99379a46f1a86e75f
BNURI <luckmon05@naver.com>
2019-12-18 03:10:06 +0900

    added connect, disconnect events

---
+++ assets/js/sockets.js
@@ -0,0 +1,12 @@
+import { handleNewUser, handleDisconnected } from "./notifications";
+
+let socket = null;
+
+export const getSocket = () => socket;
+
+export const initSockets = aSocket => {
+  const { events } = window;
+  socket = aSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handleDisconnected);
+};
